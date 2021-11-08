f777f81139655a240a3be9f1fb173f46deb2ece3
dm-marina <mysummer1404@gmail.com>
2021-11-08 11:58:05 +0200

    dm-marina

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ let num = +prompt('Enter num:');
 let pow = +prompt('Enter pow:');
 let nToPow;
 function numToPow (num , pow=1){
-    if(typeof(num)!== Number || pow === null || isNaN(pow)) {
+    if(typeof(num)!== Number || isNaN(num) || num === null) {
             return 'not a number(num)'
     } 
-     if(typeof(pow)!==Number || pow === null || isNaN(pow)){
+     if(typeof(pow)!==Number || isNaN(pow) || pow === null ){
             return 'not a number(pow)'
      }
+     if( pow!==null && num!==null){
             return num**pow;
+     }
 }
 let result = numToPow();
 alert(result);
